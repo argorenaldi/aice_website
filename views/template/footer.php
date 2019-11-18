92acfd1ad3a07b2e691cee2a2317fa0f60d914fd
argorenaldi <55913099+argorenaldi@users.noreply.github.com>
2019-11-18 13:57:23 +0700

    Add files via upload

---
+++ views/template/footer.php
@@ -0,0 +1,95 @@
+<footer id="footer">
+            <div id="footer_top">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-3 ">
+                            <a href="#"><img src="https://www.aice.co.id/upload/website/20190424094624logo_footer.png" class="logo_footer"></a>
+                        </div>
+                        <div class="col-md-9">
+                            <div class="row">
+                                <div class="col-md-4 item_footer">
+                                    <h4>GET TO KNOW US</h4>
+                                    <ul class="nav flex-column list_item">
+                                        
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">outlet</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">careers</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="col-md-4 item_footer">
+                                    <h4>CONNECT WITH US</h4>
+                                    <ul class="nav flex-column list_item">
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">contact us</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            Follow Us
+                                        </li>
+                                        
+                                    </ul>
+                                    <ul class="nav list_sosmed ">
+                                        <li class="nav-item ">
+                                            <a class="nav-link" href="https://www.facebook.com/aiceindonesia/" target="_blank"><i class="fa fa-facebook"></i></a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="https://twitter.com/aiceindonesia?lang=en" target="_blank"><i class="fa fa-twitter"></i></a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="https://www.youtube.com/channel/UCmpXhMzd41rb7zulmJaMZiQ/featured" target="_blank"><i class="fa fa-youtube"></i></a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="https://www.instagram.com/aiceindonesia/" target="_blank"><i class="fa fa-instagram"></i></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="col-md-4 item_footer">
+                                    <h4>others</h4>
+                                    <ul class="nav flex-column list_item">
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">privacy policy</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">term of use</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">FAQs</a>
+                                        </li>
+                                        
+                                        
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">ad cookies</a>
+                                        </li>
+                                    </ul>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                        
+                            
+                </div>
+            </div>
+              <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://www.aice.co.id/assets_admin/plugins/jquery/jquery.min.js "></script>
+        <script src="https://www.aice.co.id/assets_admin/plugins/bootstrap/js/popper.min.js "></script>
+        <script src="https://www.aice.co.id/assets_admin/plugins/bootstrap/js/bootstrap.min.js "></script>
+        <script src="https://www.aice.co.id/assets/plugins/bootstrap-4.2.1/dist/js/bootstrap.min.js"></script>
+        <script src="https://www.aice.co.id/assets/plugins/slick-1.8.1/slick/slick.min.js"></script>
+        <script src="https://www.aice.co.id/assets/plugins/magnific/dist/jquery.magnific-popup.min.js"></script>
+        <script src="https://www.aice.co.id/assets/plugins/flipster/dist/jquery.flipster.min.js"></script>
+   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+            <div id="footer_bottom">
+                <div class="container-fluid">
+                    <div class="col-md-12 text-center">
+                        <h6>Copyright 2019. PT Alpen Food Industry</h6>
+                    </div>
+                </div>
+            </div>
+
+        </footer>
---
+++ views/template/navbar.php
@@ -0,0 +1,54 @@
+ <link rel="stylesheet" href="<?= base_url(); ?>assets/css/aice.css">
+<header id="header" class="">
+            <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
+                <div class="container-fluid">
+                    <a class="#"><img src="https://www.aice.co.id/upload/website/20190424115327logo.png" class="logo_header"></a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main_navbar" aria-controls="main_navbar" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="main_navbar">
+                        <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Home</a>
+                            </li>
+                            <li class="nav-item ">
+                                <a class="nav-link" href="#">About</a>
+                            </li>
+                            <li class="nav-item ">
+                                <a class="nav-link" href="#">news</a>
+                            </li>
+                            <li class="nav-item ">
+                                <a class="nav-link" href="#">product</a>
+                            </li>
+                            <li class="nav-item ">
+                                <a class="nav-link" href="#">gallery</a>
+                            </li>
+                            <li class="nav-item ">
+                                <a class="nav-link" href="<?= base_url(); ?>home/contact_us">Contact Us</a>
+                            </li>
+                            <li class="nav-item ">
+                                <a class="nav-link nav_btn" href="<?= base_url(); ?>home/bereseller">Be-reseller</a>
+                            </li>
+
+                            
+                            
+                            <li class="nav-item d-none d-sm-none d-md-none d-lg-block">
+                                <a type="button" class="nav-link" data-toggle="modal" id="btn_search" data-target="#modal_search"><i class="fa fa-search"></i></a>
+                            </li>
+                            <li class="nav-item d-block d-sm-block d-md-block d-lg-none">
+                                <form action="https://www.aice.co.id/search" method="POST" role="form">
+                                    <input type="hidden" name="_token" value="YaOW96aRfaXqCJbMMCrAR0k9Y0T8gvZouqIJfWgC">
+                                    <div class="input-group">
+                                        <input type="text" class="form-control" name="title" placeholder="Search" aria-label="Search" aria-describedby="button-addon2" required="">
+                                        <div class="input-group-append">
+                                            <button class="btn btn_submit_search" type="submit" id="button-addon2"><i class="fa fa-search"></i></button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                    
+            </nav>
+        </header>
