8df5e45a3cf6b235283d533cacc58246a0304f72
argorenaldi <55913099+argorenaldi@users.noreply.github.com>
2019-11-18 13:59:14 +0700

    Add files via upload

---
+++ models/Aice_model.php
@@ -0,0 +1,43 @@
+<?php
+
+class Aice_model extends CI_model
+{
+
+
+	public function kirimLaporan()
+	{
+
+		//buat array untuk menyimpan nama,nim,email dan jurusan.
+		$data = [
+			"id_kategori" => $this->input->post('id_kategori', true),
+			"nama" => $this->input->post('nama', true),
+			"email" => $this->input->post('email', true),
+			"nohp" => $this->input->post('nohp', true),
+			"pesan" => $this->input->post('pesan', true),
+		];
+		return $this->db->insert('contactus', $data);
+		//use query builder to insert $data in table "contactus"
+
+	}
+	public function kirimDataSeller()
+	{
+
+		//buat array untuk menyimpan nama,nim,email dan jurusan.
+		$data = [
+			"nama" => $this->input->post('nama', true),
+			"email" => $this->input->post('email', true),
+			"alamat" => $this->input->post('alamat', true),
+			"no_hp" => $this->input->post('no_hp', true),
+			"nama_provinsi" => $this->input->post('nama_provinsi', true),
+			"nama_kota" => $this->input->post('nama_kota', true),
+			"nama_kecamatan" => $this->input->post('nama_kecamatan', true),
+			"kode_pos" => $this->input->post('kode_pos', true),
+		
+		];
+		return $this->db->insert('bereseller', $data);
+		//use query builder to insert $data in table "contactus"
+
+	}
+}
+
+?>
